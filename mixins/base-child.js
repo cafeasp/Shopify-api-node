da511302466ce2d22cf1787eae0ca1ef7fb50582
Victor Pacheco <vpacheco99@gmail.com>
2024-03-24 10:30:01 -0400

    console log create url

--- mixins/base-child.js
+++ mixins/base-child.js
@@ -33,6 +33,7 @@ const baseChild = {
    */
   create(parentId, params) {
     const url = this.buildUrl(parentId);
+    console.log('url',url);
     return this.shopify.request(url, 'POST', this.key, params);
   },
 
